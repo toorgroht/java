a3cc7608b36015be6b2d2f0b0d6f4c33257a29e6
toorgroht <121013162+toorgroht@users.noreply.github.com>
2022-12-19 14:30:05 +0530

    Add files via upload

---
+++ Multiple.java
@@ -0,0 +1,18 @@
+import java.util.*;
+class Multiple
+{
+static public void main(String[] args)
+{
+Scanner s=new Scanner(System.in);
+int b,n;
+System.out.println("enter an number");
+n=s.nextInt();
+System.out.println("enter an multiple");
+b=s.nextInt();
+for(int i=1;i<=n;i++)
+{
+System.out.println(i*b);
+}
+}
+}
+
